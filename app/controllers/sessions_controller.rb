e646f05b9f05b7d3236bc9abe01f9b1e833b14f7
jacksonohallo <jacksonohallo@gmail.com>
2022-10-06 16:59:53 +0300

    done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,21 @@
+
 class SessionsController < ApplicationController
+    class SessionsController < ApplicationController
+#Login
+def create
+
+   def create
+        user=User.find_by(username: params[:username])
+        session[:user_id] = user.id
+        render json: user
+    end
+
+  #LOGOUT
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+
+end
+
 end
